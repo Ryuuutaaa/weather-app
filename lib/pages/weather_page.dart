214087a509f548f9e4abdc582aeed66f88cd1ce7
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-17 19:21:54 +0800

    fixed : error debugging

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -10,6 +10,6 @@ class WeatherPage extends StatefulWidget {
 class _WeatherPageState extends State<WeatherPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold();
   }
 }
