420cbe5457e721744f77920d0746b4978fb68a48
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-18 18:06:26 +0800

    feat : add api

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -4,4 +4,12 @@ class Weather {
   final String mainConditional;
 
   Weather({required this.cityName, required this.temperature, required this.mainConditional});
+
+  factory Weather.fromJson(Map<String, dynamic> json) {
+    return Weather(
+      cityName: json["name"],
+      temperature: json["main"]["temp"].toDouble(),
+      mainConditional: json["weather"][0]["main"],
+    );
+  }
 }
---
+++ lib/services/weather_service.dart
@@ -0,0 +1,3 @@
+class WeatherService {
+  static const BASE_URL = "https://api.openweathermap.org/data/3.0/weather/";
+}
