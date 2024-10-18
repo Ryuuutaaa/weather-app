76d945c5c6c8c003adf41c02eb39a6e0da00d3ae
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-18 18:27:34 +0800

    fixed : error in weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -6,7 +6,7 @@ import 'package:weathering/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"; // Perbaiki endpoint
+  static const BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
   final String apiKey;
 
   WeatherService(this.apiKey);
@@ -15,28 +15,35 @@ class WeatherService {
     final response = await http.get(Uri.parse("$BASE_URL?q=$cityName&appid=$apiKey&units=metric"));
 
     if (response.statusCode == 200) {
-      return Weather.fromJson(jsonDecode(response.body)); // Gunakan jsonDecode
+      return Weather.fromJson(jsonDecode(response.body));
     } else {
       throw Exception('Failed to load weather data');
     }
   }
 
   Future<String> getCurrentCity() async {
-    // get permisson from user
+    // Get permission from user
     LocationPermission permission = await Geolocator.checkPermission();
+
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
+    } else if (permission == LocationPermission.deniedForever) {
+      throw Exception("Location permissions are permanently denied, we cannot request permissions.");
     }
 
-    // fetch the current location
+    // Fetch the current location
     Position position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 
-    // convert the location into a list of placemark obhjects
-    List<Placemark> placemark = await placemarkFromCoordinates(position.latitude, position.longitude);
+    // Convert the location into a list of placemark objects
+    List<Placemark> placemarks = await placemarkFromCoordinates(position.latitude, position.longitude);
 
-    // extract the city name from the fist placemark
-    String? city = placemark[0].locality;
-
-    return city ?? "";
+    // Ensure the placemarks list is not empty
+    if (placemarks.isNotEmpty) {
+      // Extract the city name from the first placemark
+      String? city = placemarks[0].locality;
+      return city ?? "Unknown city";
+    } else {
+      throw Exception("No placemarks found for the location.");
+    }
   }
 }
