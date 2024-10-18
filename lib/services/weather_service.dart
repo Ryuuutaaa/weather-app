774ee662a11b322bfcf80b273be08a99cc54e241
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-18 18:15:48 +0800

    fixed : fixed error in weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -4,18 +4,18 @@ import 'package:weathering/models/weather_model.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  static const BASE_URL = "https://api.openweathermap.org/data/3.0/weather/";
+  static const BASE_URL = "https://api.openweathermap.org/data/2.5/weather"; // Perbaiki endpoint
   final String apiKey;
 
   WeatherService(this.apiKey);
 
   Future<Weather> getWeather(String cityName) async {
-    final response = await http.get(Url.parse("$BASE_URL?q=$cityName&appid=$apiKey&units=metric"));
+    final response = await http.get(Uri.parse("$BASE_URL?q=$cityName&appid=$apiKey&units=metric"));
 
     if (response.statusCode == 200) {
-      return Weather.fromJson(jsonEncode(response.body));
+      return Weather.fromJson(jsonDecode(response.body)); // Gunakan jsonDecode
     } else {
-      throw Exception('Failded to load weather adata');
+      throw Exception('Failed to load weather data');
     }
   }
 }
