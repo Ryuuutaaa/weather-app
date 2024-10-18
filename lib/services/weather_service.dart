d1bd407e0b0e5fc240018b5eb9ce3acaeadfba3d
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-10-18 18:14:02 +0800

    error : weather service

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,3 +1,21 @@
+import 'dart:convert';
+
+import 'package:weathering/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
 class WeatherService {
   static const BASE_URL = "https://api.openweathermap.org/data/3.0/weather/";
+  final String apiKey;
+
+  WeatherService(this.apiKey);
+
+  Future<Weather> getWeather(String cityName) async {
+    final response = await http.get(Url.parse("$BASE_URL?q=$cityName&appid=$apiKey&units=metric"));
+
+    if (response.statusCode == 200) {
+      return Weather.fromJson(jsonEncode(response.body));
+    } else {
+      throw Exception('Failded to load weather adata');
+    }
+  }
 }
